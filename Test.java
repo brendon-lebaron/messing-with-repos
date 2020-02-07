b09bc24cefa986592463fa977d08f51988b25e5a
Brendon J Lebaron <lebarobj@warhawks.ulm.edu>
2020-02-07 14:03:13 -0600

    add x2 method

--- Test.java
+++ Test.java
@@ -1,10 +1,14 @@
 public class Test{
    public static void main(String[] args){
       cout("what's up");
+      cout("\nayy: " + x2(100));
    }
    
    public static void cout(String s){
       System.out.print(s);
+   }
+   public static int x2(int i){
+      return i*2;
    }
    
 }
