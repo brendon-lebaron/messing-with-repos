c7c7b5cd1a6d390bf320df0122e15104b9fe8947
Brendon J Lebaron <lebarobj@warhawks.ulm.edu>
2020-02-07 13:59:03 -0600

    add random method

--- Test.java
+++ Test.java
@@ -1,10 +1,16 @@
+import java.lang.Math;
 public class Test{
    public static void main(String[] args){
-      cout("what's up");
+      cout("what's up " + rand(5));
    }
    
    public static void cout(String s){
       System.out.print(s);
    }
-   
+   public static int rand(int bound){
+      double r = Math.random();
+      String s = String.format("%d",bound);
+      int i = s.length();
+      return (int) (Math.pow(10,i) * r)%bound + 1;
+   }
 }
